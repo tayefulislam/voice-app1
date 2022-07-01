0dd7c22cf6d5810f593264076e94f92436fb8084
tayeful912 <kazitayefulislam12@gmail.com>
2022-07-01 22:43:47 +0600

    system 1 problem solve

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { ReactMediaRecorder } from "react-media-recorder";
 import { Routes, Route, Link } from "react-router-dom";
 
 import System1 from './Pages/System1';
-import System2 from './Pages/System2';
-import System3 from './Pages/System3';
-import System4 from './Pages/System4';
+
+
+// import System2 from './Pages/System2';
+// import System3 from './Pages/System3';
+
+// import System4 from './Pages/System4';
 
 
 function App() {
@@ -20,9 +23,11 @@ function App() {
       <Routes>
 
         <Route path='/' element={<System1></System1>}></Route>
-        <Route path='/2' element={<System2></System2>}></Route>
+
+
+        {/* <Route path='/2' element={<System2></System2>}></Route>
         <Route path='/3' element={<System3></System3>}></Route>
-        <Route path='/4' element={<System4></System4>}></Route>
+        <Route path='/4' element={<System4></System4>}></Route> */}
 
       </Routes>
 
--- src/Pages/System1.js
+++ src/Pages/System1.js
@@ -5,15 +5,6 @@ import { ReactMediaRecorder } from "react-media-recorder";
 const System1 = () => {
 
 
-    const handleSubmit = (event) => {
-
-        event.preventDefault()
-
-        const a = event.target.a.value;
-
-        console.log(a)
-
-    }
 
     return (
         <div className="App">
@@ -21,14 +12,14 @@ const System1 = () => {
             <ReactMediaRecorder
                 audio
                 render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-                    <form onSubmit={handleSubmit}>
+                    <div >
                         <p>{status}</p>
                         <button onClick={startRecording}>Start Recording</button>
                         <button onClick={stopRecording}>Stop Recording</button>
-                        <audio name='a' src={mediaBlobUrl} controls autoPlay loop />
+                        <audio src={mediaBlobUrl} controls />
 
-                        <input type="submit" value='Submit' />
-                    </form>
+
+                    </div>
                 )}
             />
 
